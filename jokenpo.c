89541da7b60fa6b143d415f617d736599a5cd786
Robsontmb <rtadeumb@hotmail.com>
2025-02-25 23:22:59 +0000

    Jokenpó

---
+++ jokenpo.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main (){
+
+int jogador,computador;
+
+srand(time(0));
+
+printf("***Jogo de jokenpó***\n");
+printf("Escolha uma opção\n");
+printf("1. Pedra\n");
+printf("2. papel\n");
+printf("3. tesoura\n");
+printf("Escolha uma opção:\n");
+scanf("%d", &jogador);
+
+computador= rand () % 3 + 1 ;
+switch(jogador) {
+    case 1:
+    printf("Jogador escolheu pedra\n");
+
+    break;
+
+    case 2:
+    printf("Jogador escolheu papel\n");
+    break;
+
+    case 3:
+    printf("Jogador escolheu tesoura\n");
+    break;
+
+    default:
+    printf("Opção inválida");
+}
+switch(computador) {
+    case 1:
+    printf("Computador escolheu pedra\n");
+    
+    break;
+
+    case 2:
+    printf("Computador escolheu papel\n");
+    break;
+
+    case 3:
+    printf("Computador escolheu tesoura\n");
+    break;
+
+}
+if(jogador == computador){
+    printf("empate\n");
+} else if (jogador == 1 && computador== 2 ){
+    printf("Computador ganhou\n");
+} else if (jogador == 1 && computador== 3 ){
+    printf("jogador ganhou\n");}
+    else if (jogador == 2 && computador== 3 ){
+        printf("computador ganhou\n");}
+    else if (jogador == 2 && computador== 1 ){
+        printf("Jogador ganhou\n");}
+    else if (jogador == 3 && computador== 1 ){
+            printf("computador ganhou\n");}
+    else if (jogador == 3 && computador== 2 ){
+            printf("Jogador ganhou\n");}
+return 0;
+}
